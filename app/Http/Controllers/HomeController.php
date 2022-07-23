d6042216318d928b4df6484ed97fa43b822048cd
dipta harimbawa <vipalkuda@gmail.com>
2022-07-24 03:15:08 +0700

    [fix] Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ParkingDetail;
+use App\Models\ParkingTransaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('pages.home.index');
+        $totalParking = ParkingTransaction::where('user_id', Auth::user()->id)->count();
+        $totalLocation = ParkingDetail::whereHas('parking_transaction', function($query){
+            $query->where('user_id', Auth::user()->id);
+        })->groupBy('parking_location_id')->count();
+        return view('pages.home.index', compact('totalParking', 'totalLocation'));
     }
 }
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -11,25 +11,17 @@
                     <div
                         class="col-12 col-xl-5 text-xl-center mb-3 mb-xl-0 d-flex align-items-center justify-content-xl-center">
                         <div class="icon-shape icon-shape-secondary rounded me-4 me-sm-0">
-                            <i class="fa-solid fa-users"></i>
+                            <i class="fa-solid fa-square-parking"></i>
                         </div>
                         <div class="d-sm-none">
-                            <h2 class="fw-extrabold h5">Refenu</h2>
-                            <h3 class="mb-1">$43,594</h3>
+                            <h2 class="fw-extrabold h5">Total Parking</h2>
+                            <h3 class="mb-1">{{number_format($totalParking, 0, ",", ".")}}</h3>
                         </div>
                     </div>
                     <div class="col-12 col-xl-7 px-xl-0">
                         <div class="d-none d-sm-block">
-                            <h2 class="h6 text-gray-400 mb-0">Revenue</h2>
-                            <h3 class="fw-extrabold mb-2">$43,594</h3>
-                        </div>
-                        <small class="d-flex align-items-center text-gray-500">
-                            Feb 1 - Apr 1,
-                            <i class="fa-solid fa-users"></i>
-                            GER
-                        </small>
-                        <div class="small d-flex mt-1">
-                            <div>Since last month <span class="text-danger fw-bolder">2%</span></div>
+                            <h2 class="h6 text-gray-400 mb-0">Total Parking</h2>
+                            <h3 class="fw-extrabold mb-2">{{number_format($totalParking, 0, ",", ".")}}</h3>
                         </div>
                     </div>
                 </div>
@@ -43,23 +35,17 @@
                     <div
                         class="col-12 col-xl-5 text-xl-center mb-3 mb-xl-0 d-flex align-items-center justify-content-xl-center">
                         <div class="icon-shape icon-shape-secondary rounded me-4 me-sm-0">
-                            <i class="fas fa-regular fa-comment "></i>
+                            <i class="fa-solid fa-location-dot"></i>
                         </div>
                         <div class="d-sm-none">
-                            <h2 class="fw-extrabold h5">Revenue</h2>
-                            <h3 class="mb-1">$43,594</h3>
+                            <h2 class="fw-extrabold h5">Total Location</h2>
+                            <h3 class="mb-1">{{number_format($totalLocation, 0, ",", ".")}}</h3>
                         </div>
                     </div>
                     <div class="col-12 col-xl-7 px-xl-0">
                         <div class="d-none d-sm-block">
-                            <h2 class="h6 text-gray-400 mb-0">Revenue</h2>
-                            <h3 class="fw-extrabold mb-2">$43,594</h3>
-                        </div>
-                        <small class="d-flex align-items-center text-gray-500">
-                            Feb 1 - Apr 1, GER
-                        </small>
-                        <div class="small d-flex mt-1">
-                            <div>Since last month <span class="text-danger fw-bolder">2%</span></div>
+                            <h2 class="h6 text-gray-400 mb-0">Total Location</h2>
+                            <h3 class="fw-extrabold mb-2">{{number_format($totalParking, 0, ",", ".")}}</h3>
                         </div>
                     </div>
                 </div>
