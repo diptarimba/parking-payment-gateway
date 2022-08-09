c4449d36e8aa95f120a6536a37207fe79c1c5996
melinda <melinda.mareta@gmail.com>
2022-08-09 23:31:04 +0700

    [fix] qris

--- resources/views/pages/checkout/index.blade.php
+++ resources/views/pages/checkout/index.blade.php
@@ -49,7 +49,7 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="staticBackdropLabel">QR Code</h5>
+          <h5 class="modal-title" id="staticBackdropLabel">QRIS</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
